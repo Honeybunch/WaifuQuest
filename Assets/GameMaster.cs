ad89e02d6179ec2f5a9d3b7261f5cefc1815e64d
Dylan Nelkin <djn5286@rit.edu>
2014-10-20 17:32:49 -0400

    Started battle state changing

---
+++ Assets/GameMaster.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameMaster : MonoBehaviour {
+
+	public Texture backgroundTexture;
+	public Texture enemySprite;
+
+	public enum GameState{
+		Map,
+		Battle
+	}
+	GameState state = GameState.Map;
+
+	void onGUI(){
+		switch(state){
+		case GameState.Map:
+			break;
+		case GameState.Battle:
+			//Draw BG
+			GUI.DrawTexture(Rect(0,0,Screen.width,Screen.height), backgroundTetxure);
+			//Draw Enemy
+			GUI.DrawTexture(Rect(Screen.width/4, 0, enemySprite.width, enemySprite.height), enemySprite, 1);
+			//Battle GUI
+			break;
+		}
+	}
+	/*
+	 * 
+	 * Battle Code
+	 * 
+	 */
+
+	/*void SetUpBattle(){
+		//Load in enemy image
+		//Reset Enemy HP
+	}
+	*/
+
+}
