1d7129c5defea95e7490d64c149d9a01a75e4382
Honeybunch <amt3824@g.rit.edu>
2014-10-28 17:23:41 -0400

    Added the ability to choose gender and pronouns in the menu

---
+++ Assets/PlayerStats.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerStats : MonoBehaviour {
+
+	[HideInInspector]
+	public string playerName;
+	[HideInInspector]
+	public string gender;
+
+	//Pronouns
+	[HideInInspector]
+	public string nominative;
+	[HideInInspector]
+	public string oblique;
+	[HideInInspector]
+	public string possessive;
+	[HideInInspector]
+	public string reflexive;
+
+	//Game stats
+	public int health;
+
+	// Use this for initialization
+	void Start ()
+	{
+		health = 10;
+
+		playerName = PlayerPrefs.GetString("name");
+		gender = PlayerPrefs.GetString("gender");
+
+		nominative = PlayerPrefs.GetString("nominative");
+		oblique = PlayerPrefs.GetString("oblique");
+		possessive = PlayerPrefs.GetString("possessive");
+		reflexive = PlayerPrefs.GetString("reflexive");
+
+		Debug.Log(playerName);
+		Debug.Log(gender);
+
+		Debug.Log(nominative);
+		Debug.Log(oblique);
+		Debug.Log(possessive);
+		Debug.Log(reflexive);
+	}
+
+}
